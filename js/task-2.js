a4310fcec674aa1d2abd2df4da63d887e1f675dd
ukrnik <isaevv2003@gmail.com>
2024-10-01 17:13:08 +0200

    HW-V1

--- js/task-2.js
+++ js/task-2.js
@@ -1,59 +1,34 @@
-const getUsersWithFriend = (users, friendName) => {return users.filter(user => user.friends.includes(friendName))};
+class Storage {
 
-
-const allUsers = [
-    {
-        name: "Moore Hensley",
-        friends: ["Sharron Pace"]
-    },
-    {
-        name: "Sharlene Bush",
-        friends: ["Briana Decker", "Sharron Pace"]
-    },
-        {
-        name: "Ross Vazquez",
-        friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
-    },
-    {
-        name: "Elma Head",
-        friends: ["Goldie Gentry", "Aisha Tran"]
-    },
-    {
-        name: "Carey Barr",
-        friends: ["Jordan Sampson", "Eddie Strong"]
-    },
-    {
-        name: "Blackburn Dotson",
-        friends: ["Jacklyn Lucas", "Linda Chapman"]
-    },
-    {
-        name: "Sheree Anthony",
-        friends: ["Goldie Gentry", "Briana Decker"]
+    constructor(params){
+        this.storage = params;
     }
-];
 
-console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
-// [
-//   {
-//     name: "Sharlene Bush",
-//     friends: ["Briana Decker", "Sharron Pace"]
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"]
-//   }
-// ]
+    getItems(){
+        return this.storage;
+    }
 
-console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
-// [
-//   {
-//     name: "Elma Head",
-//     friends: ["Goldie Gentry", "Aisha Tran"]
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"]
-//   }
-// ]
+    addItem(newItem){
+        this.storage.push(newItem);
+    }
 
-console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
+    removeItem(itemToRemove){
+        if (this.storage.includes(itemToRemove)) {
+            this.storage.splice(this.storage.indexOf(itemToRemove), 1);
+        }
+    }
+}
+
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
--- js/task-3.js
+++ js/task-3.js
@@ -1,79 +1,35 @@
-const sortByDescendingFriendCount = (users) => {return users.toSorted((a, b) => b.friends.length - a.friends.length)};
+class StringBuilder {
+    #value;
+
+    constructor(params){
+        this.#value = params;
+    }
+
+    getValue(){
+        return this.#value
+    }
+
+    padEnd(str){
+        this.#value = this.#value.padEnd(this.#value.length + 1, str);
+    }
+
+    padStart(str){
+        this.#value = this.#value.padStart(this.#value.length + 1, str);
+    }
+
+    padBoth(str){
+        this.padEnd(str)
+        this.padStart(str)
+    }
+}
 
 
-console.log(
-    sortByDescendingFriendCount([
-        {
-            name: "Moore Hensley",
-            friends: ["Sharron Pace"],
-            gender: "male"
-        },
-        {
-            name: "Sharlene Bush",
-            friends: ["Briana Decker", "Sharron Pace"],
-            gender: "female"
-        },
-        {
-            name: "Ross Vazquez",
-            friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-            gender: "male"
-        },
-        {
-            name: "Elma Head",
-            friends: ["Goldie Gentry", "Aisha Tran"],
-            gender: "female"
-        },
-        {
-            name: "Carey Barr",
-            friends: ["Jordan Sampson", "Eddie Strong"],
-            gender: "male"
-        },
-        {
-            name: "Blackburn Dotson",
-            friends: ["Jacklyn Lucas", "Linda Chapman"],
-            gender: "male"
-        },
-        {
-            name: "Sheree Anthony",
-            friends: ["Goldie Gentry", "Briana Decker"],
-            gender: "female"
-        }
-    ])
-);
-  // [
-  //   {
-  //     name: "Ross Vazquez",
-  //     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-  //     gender: "male"
-  //   },
-  //   {
-  //     name: "Sharlene Bush",
-  //     friends: ["Briana Decker", "Sharron Pace"],
-  //     gender: "female"
-  //   },
-  //   {
-  //     name: "Elma Head",
-  //     friends: ["Goldie Gentry", "Aisha Tran"],
-  //     gender: "female"
-  //   },
-  //   {
-  //     name: "Carey Barr",
-  //     friends: ["Jordan Sampson", "Eddie Strong"],
-  //     gender: "male"
-  //   },
-  //   {
-  //     name: "Blackburn Dotson",
-  //     friends: ["Jacklyn Lucas", "Linda Chapman"],
-  //     gender: "male"
-  //   },
-  //   {
-  //     name: "Sheree Anthony",
-  //     friends: ["Goldie Gentry", "Briana Decker"],
-  //     gender: "female"
-  //   },
-  //   {
-  //     name: "Moore Hensley",
-  //     friends: ["Sharron Pace"],
-  //     gender: "male"
-  //   }
-  // ]
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
